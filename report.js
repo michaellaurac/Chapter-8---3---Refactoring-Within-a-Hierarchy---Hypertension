d87b5684d6bf3ccab4b9ecc296c45d22fdb4d1cd
Michaël L <michaellaurac@gmail.com>
2022-06-12 14:51:55 +0200

    Identified that having report an instance of GenericReport was sufficient to pass the tests

--- report.js
+++ report.js
@@ -47,7 +47,7 @@ const wish = require("wish");
 const deepEqual = require("deep-equal");
 
 describe("the file", () => {
-  const report = new SpecificClientReport({ whatever: "we want", to: "add" });
+  const report = new GenericReport({ whatever: "we want", to: "add" });
 
   it("prints the additional paramaters correctly", () => {
     wish(deepEqual(report.printReport({ extra: "params" }), { whatever: "we want", to: "add", extra: "params" }));
